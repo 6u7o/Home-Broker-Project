b402e4d4c27c5e4fd3b999c9a7f35f4af698520d
6u7o <augustopinheiro19999999@gmail.com>
2022-07-24 19:59:35 -0300

    nodem

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const app = require('./src/api');
 require('dotenv').config();
 
-const PORT = process.env.API_PORT/*  || 3000 */;
+const {PORT} = process.env/*  || 3000 */;
 
 app.listen(PORT, () => console.log('ouvindo porta', PORT));
